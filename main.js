daf17edf67cda833799055195eea052f7df6f060
cwymbewe <cwymbewe@gmail.com>
2023-10-03 03:48:38 +0200

    Add function that randomly return computer choice from 3 posibble outcomes

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log("Hello Charles!");
+
+let rounds;
+function getComputerChoice(){
+  let randomOutput = Math.floor(Math.random()*3);
+  return randomOutput === 1 ? "Rock" :
+        randomOutput === 2 ? "Paper" : "Scissors;"
+//returns a number between zero and two (inclusive)
+}
+console.log(getComputerChoice());
+
+
