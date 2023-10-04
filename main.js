d81ab5dac8165ff2f62b5293b0474e3a4e19f331
cwymbewe <cwymbewe@gmail.com>
2023-10-04 18:24:38 +0200

    Add playRound function

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 console.log("Hello Charles!");
 
-let rounds;
 function getComputerChoice(){
   let randomOutput = Math.floor(Math.random()*3);
   return randomOutput === 1 ? "Rock" :
@@ -9,4 +8,14 @@ function getComputerChoice(){
 }
 console.log(getComputerChoice());
 
-
+function playRound(playerSelection, computerSelection){
+  return (playerChoice == "Rock" && computerSelection == "Paper") ? "You Lose! Paper beats Rock" :
+          (playerChoice == "Paper" && computerSelection == "Scissors") ? "You Lose! Scissors beats Paper" :
+          (playerChoice == computerSelection) ? "Tie!" :
+          "You Win! "+computerSelection+" beats "+playerSelection;
+}
+const playerSelection = prompt("Enter playerOutput");
+const playerChoice = playerSelection[0].toUpperCase().concat(playerSelection.slice(1).toLowerCase());
+const computerSelection = getComputerChoice();
+console.log(playRound());
+// console.log(playerChoice);
