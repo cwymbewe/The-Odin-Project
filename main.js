1f1b213e7da435d6d8ce7fd2d2f5bc698a94c360
cwymbewe <cwymbewe@gmail.com>
2023-10-11 22:01:16 +0200

    Edit playRound() to track of win/lose counts Add game() and alert counts and declared winner

--- main.js
+++ main.js
@@ -3,23 +3,53 @@ console.log("Hello Charles!");
 function getComputerChoice(){
   let randomOutput = Math.floor(Math.random()*3);
   return randomOutput === 1 ? "Rock" :
-        randomOutput === 2 ? "Paper" : "Scissors;"
+        randomOutput === 2 ? "Paper" : "Scissors"
 //returns a number between zero and two (inclusive)
 }
 // console.log(getComputerChoice());
 
-function playRound(playerSelection, computerSelection){
-  return (playerSelection == "Rock" && computerSelection == "Paper") || 
-        (playerSelection == "Paper" && computerSelection == "Scissors") ||
-        (playerSelection == "Scissors" && computerSelection == "Rock") ? 
-          `You Lose! ${computerSelection} beats ${playerSelection}`:
-        (playerSelection === computerSelection) ? "Tie" : 
-          `You Win! ${playerSelection} beats ${computerSelection}`;
-          
+let result;
+let winCount = 0;
+let loseCount = 0;
+let drawCount = 0;
+function playRound(){
+  
+  let playerChoice = prompt("Enter playerOutput");
+  let playerSelection = playerChoice[0].toUpperCase().concat(playerChoice.slice(1).toLowerCase());
+  let computerSelection = getComputerChoice();
+
+  console.log(`Player => ${playerSelection} \nComputer => ${computerSelection}`)
+
+  if((playerSelection == "Paper" && computerSelection == "Rock") ||
+    (playerSelection == "Scissors" && computerSelection == "Paper") ||
+    (playerSelection == "Rock" && computerSelection == "Scissors")){
+      result = `You Wins! ${playerSelection} beats ${computerSelection}`;
+      winCount += 1;
+  } else if(playerSelection === computerSelection){
+      result = `Tie!`;
+      drawCount += 1;
+  } else {
+      result = `You Lose! ${computerSelection} beats ${playerSelection}`;
+      loseCount += 1;
+  }
+  console.log(result);
 }
-const playerChoice = prompt("Enter playerOutput");
-const playerSelection = playerChoice[0].toUpperCase().concat(playerChoice.slice(1).toLowerCase());
-const computerSelection = getComputerChoice();
-console.log(`${playerSelection} and ${computerSelection}`);
-alert(`Player chose ${playerSelection}, Computer chose ${computerSelection}.\n${playRound(playerSelection, computerSelection)}`);
-console.log(playRound(playerSelection, computerSelection));
+
+function game(rounds){
+  
+  for(i=1 ; i<= rounds ; ++i ){
+    playRound()
+  }
+  console.log(`Wins: ${winCount}\nDraws:${drawCount}\nLosses: ${loseCount}`);
+  
+  if(`${winCount}` > `${loseCount}`) {
+    alert('Congrats, you won the Game!');
+  } else if(`${winCount}` < `${loseCount}`) {
+    alert('Sorry You lost the Game');
+  } else {
+    alert('It was a tie!');
+  }
+  // alert(`Wins: ${winCount}\nDraws:${drawCount}\nLosses: ${loseCount}`);
+}
+
+game(5);
