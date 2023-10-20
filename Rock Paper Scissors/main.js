d2dde450f8c260d9e1cbdd1fa338cab169dc0c74
cwymbewe <cwymbewe@gmail.com>
2023-10-20 14:58:47 +0200

    Edit to playRound() to keep track of result count Create and append a div element to the DOM

--- Rock Paper Scissors/main.js
+++ Rock Paper Scissors/main.js
@@ -1,5 +1,6 @@
 console.log("Hello Charles!");
 
+//FUNCTION TO GET RANDOM COMPUTER SELECTIONS
 function getComputerChoice(){
   let randomOutput = Math.floor(Math.random()*3);
   return randomOutput === 1 ? "Rock" :
@@ -7,6 +8,7 @@ function getComputerChoice(){
 //returns a number between zero and two (inclusive)
 }
 
+//BUTTONS THAT CALL THE PLAYROUND FUNCTION
 let playerSelection;
 let computerSelection;
 
@@ -39,25 +41,53 @@ scissorsBtn.addEventListener("click", () => {
   
 });
 
+//PLAY ROUND FUNCTION
 
-
+let playerCount = 0;
+let computerCount = 0;
+let drawCount = 0;
 
 function playRound(playerSelection, computerSelection) {
-  
+
+let result;
   console.log(`Player => ${playerSelection} \nComputer => ${computerSelection}`)
-  let result;
-  
+    
   if((playerSelection == "Paper" && computerSelection == "Rock") ||
     (playerSelection == "Scissors" && computerSelection == "Paper") ||
     (playerSelection == "Rock" && computerSelection == "Scissors")){
       result = `You Wins! ${playerSelection} beats ${computerSelection}`;
-      
+      playerCount += 1;
+
   } else if(playerSelection === computerSelection){
       result = `Tie!`;
-      
+      drawCount += 1;
   } else {
       result = `You Lose! ${computerSelection} beats ${playerSelection}`;
-      
+      computerCount += 1;
   }
   console.log(result);
+  console.log(drawCount + ":" + playerCount + ":" + computerCount );
 }
+
+//CREATE DIV ELEMENT
+
+const div = document.createElement("div");
+div.innerHTML = `<h2>Charles' Score: ${playerCount}</h2>
+                <h2>Computer's Score: ${computerCount}</h2>
+                <h2>Draw Count: ${drawCount}</h2>`;
+document.body.appendChild(div);
+
+/*
+function finalResult(){
+  if((playerCount > computerCount) && (playerCount + drawCount + computerCount == 5)){
+    alert(`Congratulations! You won the game with a score of ${playerCount}.`);
+  } else {
+    alert(`Sorry you lost the game with a score of ${computerCount}.`);
+  }
+  return true;
+
+}
+
+finalResult();
+
+*/
